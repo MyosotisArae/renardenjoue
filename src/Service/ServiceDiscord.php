1ec1ea620d119e4086cf0691c51ffa03ef2d67ed
Carla <myosotis.arae@gmail.com>
2021-09-21 13:50:42 +0200

    ServiceDiscord ajout de la commande read pour afficher les prochains évts

--- src/Service/ServiceDiscord.php
+++ src/Service/ServiceDiscord.php
@@ -9,20 +9,33 @@ use Discord\Parts\Embed;
 use Discord\Factory\Factory;
 use Discord\WebSockets\Event;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Console\Command\Command;
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 use App\Discord\MessagesBot;
 
-class ServiceDiscord
+class ServiceDiscord extends Command
 {
+    // Parce que c'est une commande, il lui faut un nom :
+    protected static $defaultName = 'app:read';
     private $botId;
     private $discord;
     private $messages;
+    private $container;
 
-    public function __construct(string $token, string $botId) {	
+    public function __construct(string $token, string $botId,ContainerInterface $container) {
 	    $this->discord = new Discord([
 		    'token' => $token,
 	    ]);
         $this->botId = $botId;
+        // Pour gérer les messages qu'envoie le bot :
         $this->messages = new MessagesBot($botId, $this->discord);
+        // Pour accéder à la base de données :
+        parent::__construct();
+        $this->container = $container;
     }
 
     public function discordOn() {
@@ -36,5 +49,21 @@ class ServiceDiscord
 		$this->discord->run();
     }
 
+    // Parce que c'est une commande, il faut implémenter execute :
+    public function execute(InputInterface $input, OutputInterface $output): int {
+        $em = $this->container->get('doctrine')->getManager();
+        $nextEvts = $em->getRepository('App:Evenements')
+                       ->getNextEvts();
+        echo "Prochains evts :", PHP_EOL;
+        var_dump($nextEvts);
+        return Command::SUCCESS;
+
+        // Some error happened during the execution
+        // return Command::FAILURE;
+
+        // Incorrect command usage; e.g. invalid options
+        // or missing arguments
+        // return Command::INVALID
+    }
 }
 ?>
